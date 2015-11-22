05e4da29ba1a7fb4f73bea143fc8e7e9603429df
Yukino Ikegami <yukino0131@me.com>
2015-11-22 18:25:14 +0900

    refine test

--- tests/test_kpy.py
+++ tests/test_kpy.py
@@ -1,19 +1,18 @@
+import glob
 import os
 import json
 import kpy
 from nose.tools import assert_equals
 
 
-JSONS = ('ymobile', 'docomo', 'docomo_android', 'softbank',
-         'android_au', 'softbank_android')
 OSX_UA = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/'
           '534.24 (KHTML, like Gecko) Chrome/11.0.696.71 Safari/534.24')
 
 
 def test_extract():
-    test_dir = os.path.abspath(os.path.dirname(__file__))
-    json_open = lambda x: json.load(open(os.path.join(test_dir, x + '.json')))
-    for d in map(json_open, JSONS):
+    json_pattern = os.path.join(os.path.abspath(os.path.dirname(__file__)), '*.json')
+    load_json = lambda x: json.load(open(x))
+    for d in map(load_json, glob.glob(json_pattern)):
         for (name, uas) in d.items():
             for ua in uas:
                 got = kpy.extract(ua)
