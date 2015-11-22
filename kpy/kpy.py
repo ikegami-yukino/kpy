70668bf6c0d12c2998030bbf68a22b62a9fdd4f9
Yukino Ikegami <yukino0131@me.com>
2015-11-22 18:14:28 +0900

    update kpy.py

--- kpy/kpy.py
+++ kpy/kpy.py
@@ -2,9 +2,10 @@ import re
 
 
 REGEXES = map(re.compile, (' (?P<name>[A-Z]{6,7} [A-Z0-9]{3,9}) ',
+                           '\((?P<name>[A-Z0-9\-]{4,8}[lpsu]{,4})\)',
+                           '^(J-PHONE|Vodafone|SoftBank)/\d.\d/V?(?P<name>[A-Z0-9\-f]{3,})(/|_?[a-c]|$)',
                            ' (Sony)?(Ericsson)?(?P<name>[A-Z\-]{,5}[0-9]{2,}[A-Z\-0-9]{,6}) ',
                            '/(?P<name>[A-Z0-9\-]{4,9}[eflpsu]{,4})/',
-                           '\((?P<name>[A-Z0-9\-]{4,8}[lpsu]{,4})\)',
                            '(?P<name>Galaxy Nexus)',
                            '(?P<name>SmartTVStick)',
                            '/(?P<name>[A-Z\-]{,5}[0-9][A-Z\-0-9]{,4})\)',
@@ -16,7 +17,9 @@ REGEXES = map(re.compile, (' (?P<name>[A-Z]{6,7} [A-Z0-9]{3,9}) ',
                            '(?P<name>Nexus [0-9a-zA-Z]+)',
                            '(?P<name>BlackBerry ?[0-9]*)[/;]',
                            ' (?P<name>((Nokia)|(Series))[\-a-zA-Z0-9]+)/',
-                           '\A(?P<name>[A-Z0-9\-]{3,})[/;]'))
+                           '\A(?P<name>[A-Z0-9\-]{3,})[/;]',
+                           '/(?P<name>[A-Z]{,2}[0-9]{3}i[stCGPRSWM]{0,3})(/|$)',
+                           '/(?P<name>[A-Z]{,2}[0-9]{3}[mv])(/|$)'))
 REGEXES = list(REGEXES)
 OTHER_MODELS = ('dtab01', 'd-01G')
 
